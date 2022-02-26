c342febb1bb31f023cf6cbd0970d4d9533bbba7f
AntonBelski <anthony.belski@gmail.com>
2022-02-26 16:02:27 +0300

    add another solution for the puzzle 448

--- 448_find_all_numbers_disappeared_in_an_array.py
+++ 448_find_all_numbers_disappeared_in_an_array.py
@@ -1,5 +1,6 @@
 class Solution:
-    def findDisappearedNumbers(self, nums):
+    def findDisappearedNumbers2(self, nums):
+        # solution 1 with negative integers, TC - O(n), SC - O(1)
         n = len(nums)
         for i in range(n):
             elem = abs(nums[i]) - 1
@@ -11,8 +12,26 @@ class Solution:
                 results.append(i + 1)
         return results
 
+    def findDisappearedNumbers(self, nums):
+        # solution 1 with Cyclic Sort, TC - O(n), SC - O(1)
+        n = len(nums)
+        result = []
+
+        for i in range(n):
+            while nums[i] != nums[nums[i] - 1]:
+                nums[nums[i] - 1], nums[i] = nums[i], nums[nums[i] - 1]
+
+        for i in range(n):
+            if i + 1 != nums[i]:
+                result.append(i + 1)
+
+        return result
+
 
 if __name__ == '__main__':
     solution = Solution()
-    result = solution.findDisappearedNumbers([4, 3, 2, 7, 8, 2, 3, 8])
+    nums = [4, 3, 2, 7, 8, 2, 3, 1]
+    result = solution.findDisappearedNumbers2(nums.copy())
+    print(result)
+    result = solution.findDisappearedNumbers(nums.copy())
     print(result)
