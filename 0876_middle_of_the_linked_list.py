a880969c70c4791b0954b5f0b41d2c3eb0f95176
AntonBelski <anthony.belski@gmail.com>
2022-03-05 13:11:48 +0300

    add solution for the problem 876

---
+++ 0876_middle_of_the_linked_list.py
@@ -0,0 +1,31 @@
+from typing import Optional
+
+
+# Definition for singly-linked list.
+class ListNode:
+    def __init__(self, val=0, next=None):
+        self.val = val
+        self.next = next
+
+
+class Solution:
+    def middleNode(self, head: Optional[ListNode]) -> Optional[ListNode]:
+        slow = head
+        fast = head
+
+        while fast and fast.next:
+            slow = slow.next
+            fast = fast.next.next
+
+        return slow
+
+
+if __name__ == '__main__':
+    solution = Solution()
+    list_node_fifth = ListNode(val=5)
+    list_node_fourth = ListNode(val=4, next=list_node_fifth)
+    list_node_third = ListNode(val=3, next=list_node_fourth)
+    list_node_second = ListNode(val=2, next=list_node_third)
+    list_node_first = ListNode(val=1, next=list_node_second)
+    middle_node = solution.middleNode(list_node_first)
+    print(f'Head, value = {middle_node.val}, next node value = {middle_node.next.val}')
