9cf7b38b1646e14aab56e4bacb9610b02fd5dea0
AntonBelski <anthony.belski@gmail.com>
2022-05-02 12:17:15 +0300

    add second solution for the problem 658

--- 0658_find_k_closest_elements.py
+++ 0658_find_k_closest_elements.py
@@ -2,7 +2,9 @@ from typing import List
 
 
 class Solution:
-    def findClosestElements(self, arr: List[int], k: int, x: int) -> List[int]:
+    def findClosestElements2(self, arr: List[int], k: int, x: int) -> List[int]:
+        # First solution with Two Pointers
+        # Time Complexity - O(n - k), Space Complexity - O(1)
         start = 0
         end = len(arr) - 1
 
@@ -14,11 +16,40 @@ class Solution:
 
         return arr[start:end + 1]
 
+    def findClosestElements(self, arr: List[int], k: int, x: int) -> List[int]:
+        # Second solution with Binary Search + Two Pointers
+        # Time Complexity - O(k + log(n)), Space Complexity - O(1)
+        lo, hi = 0, len(arr) - 1
+        while lo != hi:
+            mid = (lo + hi) // 2
+            if x <= arr[mid]:
+                hi = mid
+            else:
+                lo = mid + 1
+
+        if lo > 0 and abs(arr[lo - 1] - x) <= abs(arr[lo] - x):
+            lo -= 1
+
+        start = end = lo
+        while end + 1 - start != k:
+            if start == 0:
+                end += 1
+            elif end == len(arr) - 1:
+                start -= 1
+            elif abs(arr[start - 1] - x) <= abs(arr[end + 1] - x):
+                start -= 1
+            else:
+                end += 1
+
+        return arr[start:end + 1]
+
 
 if __name__ == '__main__':
     solution = Solution()
     arr = [1, 2, 3, 4, 5]
     k = 4
     x = 3
+    result = solution.findClosestElements2(arr, k, x)
+    print(result)
     result = solution.findClosestElements(arr, k, x)
     print(result)
