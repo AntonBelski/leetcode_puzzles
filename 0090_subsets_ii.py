b078d6206f9c9c2bd82bee7c9264e4c9bae990c2
AntonBelski <anthony.belski@gmail.com>
2022-05-16 15:20:49 +0300

    refactor solution for the problem 90

--- 0090_subsets_ii.py
+++ 0090_subsets_ii.py
@@ -2,14 +2,14 @@ from typing import List
 
 
 class Solution:
-    def recursive_sebsets(self, nums, index=0):
+    def recursive_subsets(self, nums, index=0):
         for i in range(index, len(nums)):
             self.subset.append(nums[i])
             subset_tuple = tuple(self.subset)
             if subset_tuple not in self.subsets:
                 self.result.append(self.subset[:])
                 self.subsets.add(subset_tuple)
-            self.recursive_sebsets(nums, i + 1)
+            self.recursive_subsets(nums, i + 1)
             self.subset.pop()
 
     def subsetsWithDup(self, nums: List[int]) -> List[List[int]]:
@@ -18,23 +18,23 @@ class Solution:
         self.result = [[]]
         self.subset = []
         self.subsets = set()
-        self.recursive_sebsets(nums)
+        self.recursive_subsets(nums)
         return self.result
 
-    def recursive_sebsets2(self, nums, result, subset=[], index=0):
+    def recursive_subsets2(self, nums, result, subset=[], index=0):
         for i in range(index, len(nums)):
             if i != index and nums[i] == nums[i - 1]:
                 continue
             subset.append(nums[i])
             result.append(subset[:])
-            self.recursive_sebsets2(nums, result, subset, i + 1)
+            self.recursive_subsets2(nums, result, subset, i + 1)
             subset.pop()
 
     def subsetsWithDup2(self, nums: List[int]) -> List[List[int]]:
         # Smart Solution, Time Complexity - O(n * 2^n), Space Complexity - O(n), hard to fully understand
         nums.sort()
         result = [[]]
-        self.recursive_sebsets2(nums, result)
+        self.recursive_subsets2(nums, result)
         return result
 
 
