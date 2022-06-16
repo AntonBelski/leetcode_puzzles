608e43604982b5ee954f4821581018ae04542afe
AntonBelski <anthony.belski@gmail.com>
2022-06-16 17:13:10 +0300

    refactor solution for the problem 2299

--- 2299_strong_password_checker_ii.py
+++ 2299_strong_password_checker_ii.py
@@ -5,11 +5,11 @@ class Solution:
     def strongPasswordCheckerII(self, st) -> bool:
         r = [False] * 5 + [True]
         for i, c in enumerate(st):
-            r[0] = any([r[0], +(c.islower())])
-            r[1] = any([r[1], +(c.isupper())])
-            r[2] = any([r[2], +(c.isdigit())])
-            r[3] = any([r[3], +(c in "!@#$%^&*()-+")])
-            r[4] = any([r[4], +(i > 6)])
+            r[0] = any([r[0], c.islower()])
+            r[1] = any([r[1], c.isupper()])
+            r[2] = any([r[2], c.isdigit()])
+            r[3] = any([r[3], c in "!@#$%^&*()-+"])
+            r[4] = any([r[4], i > 6])
             if i > 0 and st[i - 1] == st[i]:
                 r[5] = False
 
