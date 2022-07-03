70f8f2490b220eac8f28df2be301949e3b19e30c
AntonBelski <anthony.belski@gmail.com>
2022-07-04 00:34:29 +0300

    add solution for the problem 1926

---
+++ 1926_nearest_exit_from_entrance_in_maze.py
@@ -0,0 +1,32 @@
+from collections import deque
+from typing import List
+
+
+class Solution:
+    def nearestExit(self, maze: List[List[str]], entr: List[int]) -> int:
+        rows, cols = len(maze), len(maze[0])
+        deq = deque()
+        deq.append([entr[0], entr[1], -1])
+
+        while deq:
+            r, c, dist = deq.popleft()
+            if not (0 <= r < rows and 0 <= c < cols):
+                if dist > 0:
+                    return dist
+                continue
+            if maze[r][c] == '+':
+                continue
+
+            maze[r][c] = '+'
+            for _r, _c in ((0, 1), (0, -1), (1, 0), (-1, 0)):
+                deq.append([r + _r, c + _c, dist + 1])
+
+        return -1
+
+
+if __name__ == '__main__':
+    solution = Solution()
+    maze = [["+", "+", ".", "+"], [".", ".", ".", "+"], ["+", "+", "+", "."]]
+    entrance = [1, 2]
+    result = solution.nearestExit(maze, entrance)
+    print(result)
