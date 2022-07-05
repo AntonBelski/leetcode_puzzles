2297d695815f17411eeb53fd9fa329ce14ec0af3
AntonBelski <anthony.belski@gmail.com>
2022-07-06 00:05:47 +0300

    update solution for the problem 547

--- 0547_number_of_provinces.py
+++ 0547_number_of_provinces.py
@@ -2,49 +2,48 @@ from typing import List
 
 
 class UnionFind:
-    def __init__(self, nums):
-        self.nodes = {}
-        self.sizes = {}
+    def __init__(self, n):
+        self.nodes = []
+        self.sizes = []
+        self.provinces = n
 
-        for i in range(nums):
-            self.nodes[i] = i
-            self.sizes[i] = 1
-
-    def root(self, p):
-        while p != self.nodes[p]:
-            self.nodes[p] = self.nodes[self.nodes[p]]
-            p = self.nodes[p]
-
-        return p
+        for i in range(n):
+            self.nodes.append(i)
+            self.sizes.append(1)
 
     def union(self, p, q):
         root_p = self.root(p)
         root_q = self.root(q)
 
-        if self.nodes[root_p] > self.nodes[root_q]:
+        if root_p == root_q:
+            return
+
+        self.provinces -= 1
+
+        if self.sizes[root_p] > self.sizes[root_q]:
             self.nodes[root_q] = root_p
             self.sizes[root_p] += self.sizes[root_q]
         else:
             self.nodes[root_p] = root_q
             self.sizes[root_q] += self.sizes[root_p]
 
+    def root(self, p):
+        while p != self.nodes[p]:
+            p, self.nodes[p] = self.nodes[p], self.nodes[self.nodes[p]]
+        return p
+
 
 class Solution:
-    def findCircleNum(self, grid: List[List[int]]) -> int:
-        provinces = set()
-        columns_len = len(grid)
-        union_find = UnionFind(len(grid))
+    def findCircleNum(self, matrix: List[List[int]]) -> int:
+        n = len(matrix)
+        union_find = UnionFind(n)
 
-        for i in range(columns_len):
-            for j in range(columns_len):
-                if grid[i][j] == 1 and i != j:
-                    union_find.union(i, j)
+        for p in range(n):
+            for q in range(p + 1, n):
+                if matrix[p][q] == 1:
+                    union_find.union(p, q)
 
-        for i in range(columns_len):
-            root_elem = union_find.root(i)
-            provinces.add(root_elem)
-
-        return len(provinces)
+        return union_find.provinces
 
 
 if __name__ == '__main__':
