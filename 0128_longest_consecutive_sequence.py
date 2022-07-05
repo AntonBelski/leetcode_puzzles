55db497a5b94f8d96709a292e052595a2f622f00
AntonBelski <anthony.belski@gmail.com>
2022-07-06 00:18:32 +0300

    update solution for the problem 128

--- 0128_longest_consecutive_sequence.py
+++ 0128_longest_consecutive_sequence.py
@@ -37,7 +37,7 @@ class UnionFind:
 
 class Solution:
     def longestConsecutive1(self, nums: List[int]) -> int:
-        # First solution with Union Find
+        # First solution with Union Find (My Solution)
         # Time Complexity - O(n + m * log*(n)), * -> this function veryyy close to constant O(1)
         # Space Complexity - O(n)
         union_find = UnionFind(nums)
@@ -57,7 +57,37 @@ class Solution:
         return max_size
 
     def longestConsecutive2(self, nums: List[int]) -> int:
-        # Second solution with sets
+        # Second solution with Sets (My Solution)
+        # Time Complexity - O(n), Space Complexity - O(n)
+        uniq_nums = set(nums)
+        used_nums = set()
+        result = 0
+
+        for num in nums:
+            if num in used_nums:
+                continue
+
+            curr_res = 1
+            used_nums.add(num)
+
+            left_num = num - 1
+            while left_num in uniq_nums:
+                curr_res += 1
+                used_nums.add(left_num)
+                left_num -= 1
+
+            right_num = num + 1
+            while right_num in uniq_nums:
+                curr_res += 1
+                used_nums.add(right_num)
+                right_num += 1
+
+            result = max(result, curr_res)
+
+        return result
+
+    def longestConsecutive3(self, nums: List[int]) -> int:
+        # Second solution with sets (From Leetcode Solutions)
         # Time Complexity - O(n), Space Complexity - O(n)
         longest_streak = 0
         nums_set = set(nums)
@@ -79,7 +109,9 @@ class Solution:
 if __name__ == '__main__':
     solution = Solution()
     nums = [100, 4, 200, 1, 3, 2]
-    res1 = solution.longestConsecutive1(nums)
-    res2 = solution.longestConsecutive2(nums)
-    print(res1)
-    print(res2)
+    result = solution.longestConsecutive1(nums)
+    print(result)
+    result = solution.longestConsecutive2(nums)
+    print(result)
+    result = solution.longestConsecutive3(nums)
+    print(result)
