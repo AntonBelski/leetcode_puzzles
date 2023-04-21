c956168965bd07f0269532a196d12f4ce08dc60b
AntonBelski <anthony.belski@gmail.com>
2023-04-21 16:34:51 +0300

    add another solution for the problem 15

--- 0015_3sum.py
+++ 0015_3sum.py
@@ -28,9 +28,31 @@ class Solution:
 
         return [list(s) for s in result]
 
+    def threeSum2(self, nums: List[int]) -> List[List[int]]:
+        nums.sort()
+        sets = set()
+
+        for i in range(len(nums) - 2):
+            j, k = i + 1, len(nums) - 1
+            while j < k:
+                three_sum = nums[i] + nums[j] + nums[k]
+                if three_sum == 0:
+                    new_tuple = tuple([nums[i], nums[j], nums[k]])
+                    sets.add(new_tuple)
+                    j += 1
+                    k -= 1
+                elif three_sum > 0:
+                    k -= 1
+                else:
+                    j += 1
+
+        return sets
+
 
 if __name__ == '__main__':
     solution = Solution()
     nums = [-1, 0, 1, 2, -1, -4]
     res = solution.threeSum(nums)
+    print(res)
+    res = solution.threeSum2(nums)
     print(res)
